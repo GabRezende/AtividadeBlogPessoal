98fb5dce706bc057e4517328e7842678eb99d0a2
unknown <contatogabrezende@gmail.com>
2024-06-04 21:11:35 -0300

    Criação de segurança e usuario

--- src/main/java/com/generation/blog/security/UserDetailsServiceImpl.java
+++ src/main/java/com/generation/blog/security/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.generation.blog.security;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,18 +15,19 @@ import com.generation.blog.repository.UsarioRepository;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-
+	
+	@Autowired
 	private UsarioRepository usuarioRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-	 Optional <Usuario> usuario = usuarioRepository.findByUsuario(username);
-	 
-	  if (usuario.isPresent()) {
-		  return new UserDetailsImpl(usuario.get());
-	  }else {
-		  throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-	  }
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(userName);
+
+		if (usuario.isPresent())
+			return new UserDetailsImpl(usuario.get());
+		else
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+			
 	}
 }
--- src/main/java/com/generation/blog/service/UsuarioService.java
+++ src/main/java/com/generation/blog/service/UsuarioService.java
@@ -1,5 +1,4 @@
 package com.generation.blog.service;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +10,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.generation.blog.model.Usuario;
 import com.generation.blog.model.UsuarioLogin;
+import com.generation.blog.model.Usuario;
 import com.generation.blog.repository.UsarioRepository;
 import com.generation.blog.security.JwtService;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsarioRepository usuarioRepository;
-	
+
 	@Autowired
-	private JwtService jwtService;
-	
-	private AuthenticationManager authenticationManager;
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
 	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
 
@@ -35,55 +35,73 @@ public class UsuarioService {
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
 
 		return Optional.of(usuarioRepository.save(usuario));
+	
 	}
+
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+		
+		if(usuarioRepository.findById(usuario.getId()).isPresent()) {
+
+			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
+
+			if ( (buscaUsuario.isPresent()) && ( buscaUsuario.get().getId() != usuario.getId()))
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+			return Optional.ofNullable(usuarioRepository.save(usuario));
+			
+		}
+
+		return Optional.empty();
 	
-	
-	
-	
+	}	
+
+	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
+        
+        // Gera o Objeto de autenticação
+		var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha());
+		
+        // Autentica o Usuario
+		Authentication authentication = authenticationManager.authenticate(credenciais);
+        
+        // Se a autenticação foi efetuada com sucesso
+		if (authentication.isAuthenticated()) {
+
+            // Busca os dados do usuário
+			Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
+
+            // Se o usuário foi encontrado
+			if (usuario.isPresent()) {
+
+                // Preenche o Objeto usuarioLogin com os dados encontrados 
+			   usuarioLogin.get().setId(usuario.get().getId());
+                usuarioLogin.get().setNome(usuario.get().getNome());
+                usuarioLogin.get().setFoto(usuario.get().getFoto());
+                usuarioLogin.get().setToken(gerarToken(usuarioLogin.get().getUsuario()));
+                usuarioLogin.get().setSenha("");
+				
+                 // Retorna o Objeto preenchido
+			   return usuarioLogin;
+			
+			}
+
+        } 
+            
+		return Optional.empty();
+
+    }
+
 	private String criptografarSenha(String senha) {
 
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
 		return encoder.encode(senha);
-}
-	
-	
-	
-public Optional<Usuario> atualizarUsuario(Usuario usuario) {
-		
-		if(usuarioRepository.findById(usuario.getId()).isPresent()) {
-			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
 
-			if ( (buscaUsuario.isPresent()) && ( buscaUsuario.get().getId() != usuario.getId()))
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
-			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+	}
 
-			return Optional.ofNullable(usuarioRepository.save(usuario));
-}
-		return Optional.empty();
-}
-public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
+	private String gerarToken(String usuario) {
+		return "Bearer " + jwtService.generateToken(usuario);
+	}
 
-	var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha());
-	Authentication authentication = authenticationManager.authenticate(credenciais);
-	
-	if (authentication.isAuthenticated()) {
-		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
-		
-		if (usuario.isPresent()) {
-			   usuarioLogin.get().setId(usuario.get().getId());
-               usuarioLogin.get().setNome(usuario.get().getNome());
-               usuarioLogin.get().setFoto(usuario.get().getFoto());
-               usuarioLogin.get().setToken(gerarToken(usuarioLogin.get().getUsuario()));
-               usuarioLogin.get().setSenha("");
-				
-			   return usuarioLogin;
-		}
-       }   return Optional.empty();
 }
-private String gerarToken(String usuario) {
-	return "Bearer " + jwtService.generateToken(usuario);
-}
-}
-
-
